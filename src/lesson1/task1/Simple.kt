d4005ea55633fa7396809921c6ef8abbe9dc4c86
Nika <nikanikakrama@gmail.com>
2020-11-01 14:43:28 +0200

    Lesson 1: 2 простых, 3 тривильных

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -5,37 +5,27 @@ package lesson1.task1
 import kotlin.math.*
 
 /**
- * Пример
- *
  * Вычисление квадрата целого числа
  */
 fun sqr(x: Int) = x * x
 
 /**
- * Пример
- *
  * Вычисление квадрата вещественного числа
  */
 fun sqr(x: Double) = x * x
 
 /**
- * Пример
- *
  * Вычисление дискриминанта квадратного уравнения
  */
 fun discriminant(a: Double, b: Double, c: Double) = sqr(b) - 4 * a * c
 
 /**
- * Пример
- *
  * Поиск одного из корней квадратного уравнения
  */
 fun quadraticEquationRoot(a: Double, b: Double, c: Double) =
     (-b + sqrt(discriminant(a, b, c))) / (2 * a)
 
 /**
- * Пример
- *
  * Поиск произведения корней квадратного уравнения
  */
 fun quadraticRootProduct(a: Double, b: Double, c: Double): Double {
@@ -49,8 +39,7 @@ fun quadraticRootProduct(a: Double, b: Double, c: Double): Double {
  * Пример главной функции
  */
 fun main() {
-    val x1x2 = quadraticRootProduct(1.0, 13.0, 42.0)
-    println("Root product: $x1x2")
+    println(travelMinutes(9,25,13,1))
 }
 
 /**
@@ -59,7 +48,11 @@ fun main() {
  * Пользователь задает время в часах, минутах и секундах, например, 8:20:35.
  * Рассчитать время в секундах, прошедшее с начала суток (30035 в данном случае).
  */
-fun seconds(hours: Int, minutes: Int, seconds: Int): Int = TODO()
+fun seconds(hours: Int, minutes: Int, seconds: Int): Int {
+    val hoursToSeconds = hours * 60 * 60
+    val minutesToSeconds = minutes * 60
+    return hoursToSeconds + minutesToSeconds + seconds
+}
 
 /**
  * Тривиальная
@@ -68,7 +61,14 @@ fun seconds(hours: Int, minutes: Int, seconds: Int): Int = TODO()
  * Определить длину того же отрезка в метрах (в данном случае 18.98).
  * 1 сажень = 3 аршина = 48 вершков, 1 вершок = 4.445 см.
  */
-fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double = TODO()
+fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double {
+
+    val smSagenes = 4.445 * 48 * sagenes
+    val smArshins = 48 * 4.445 / 3 * arshins
+    val smVershoks = 4.445 * vershoks
+    val metr = (smSagenes + smArshins + smVershoks)/100
+    return metr
+}
 
 /**
  * Тривиальная
@@ -76,7 +76,12 @@ fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double = TODO()
  * Пользователь задает угол в градусах, минутах и секундах (например, 36 градусов 14 минут 35 секунд).
  * Вывести значение того же угла в радианах (например, 0.63256).
  */
-fun angleInRadian(deg: Int, min: Int, sec: Int): Double = TODO()
+fun angleInRadian(deg: Int, min: Int, sec: Int): Double {
+     val degSum = deg + (sec +  min / 60)/60
+    return degSum * 3.141592653589793238462643 / 180
+}
+
+
 
 /**
  * Тривиальная
@@ -93,7 +98,6 @@ fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double = TODO()
  * Определить третью цифру справа в этом числе (в данном случае 8).
  */
 fun thirdDigit(number: Int): Int = TODO()
-
 /**
  * Простая
  *
@@ -101,7 +105,11 @@ fun thirdDigit(number: Int): Int = TODO()
  * прибыл на станцию назначения в h2 часов m2 минут того же дня (например в 13:01).
  * Определите время поезда в пути в минутах (в данном случае 216).
  */
-fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minutesArrive: Int): Int = TODO()
+fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minutesArrive: Int): Int {
+    val depart = hoursDepart * 60 + minutesDepart
+    val arrive = hoursArrive * 60 + minutesArrive
+   return arrive - depart
+}
 
 /**
  * Простая
@@ -110,7 +118,11 @@ fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minute
  * Сколько денег будет на счету через 3 года (с учётом сложных процентов)?
  * Например, 100 рублей под 10% годовых превратятся в 133.1 рубля
  */
-fun accountInThreeYears(initial: Int, percent: Int): Double = TODO()
+fun accountInThreeYears(initial: Int, percent: Int): Double {
+    val procent = initial / 100 * percent
+    return (initial + (procent * 3)).toDouble()
+
+}
 
 /**
  * Простая
